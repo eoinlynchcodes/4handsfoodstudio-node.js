5b1751fa8cb5052bc837ec7d354d995eaff7f388
eoinlynchcodes <eoin95lynch@gmail.com>
2020-09-02 13:01:50 +0100

    Going to Postgres.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = require('./api/server')
 
 const PORT = process.env.PORT || 3333
 
+// console.log(process.env.DATABASE_URL);
+
 // watch for connections on port 5000
 app.listen(PORT, () =>
   console.log(`The server runs on Port #${PORT}`)
--- knexfile.js
+++ knexfile.js
@@ -17,7 +17,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DB_URL,
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './data/migrations',
     },
