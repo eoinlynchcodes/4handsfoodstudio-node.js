969fb27614cb35f12b6da53951d3893f623185cd
eoinlynchcodes <eoin95lynch@gmail.com>
2020-09-03 11:05:03 +0100

    Procfile added.

--- knexfile.js
+++ knexfile.js
@@ -21,6 +21,8 @@ module.exports = {
     migrations: {
       directory: './data/migrations',
     },
-    seeds: { directory: './data/seeds' },
+    seeds: { 
+      directory: './data/seeds' 
+    },
   },
 };
